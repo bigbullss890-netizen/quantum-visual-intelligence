2ed80f67bdc42d4d005035f1b94ca2674cb3c1f6
bigbullss890-netizen <bigbullss890@gmail.com>
2026-01-18 03:34:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <--- MAKE SURE THIS LINE EXISTS
+import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// -----------------------------------------------------------
+// 🔴 PASTE YOUR CLERK PUBLISHABLE KEY HERE FROM CLERK.COM
+const PUBLISHABLE_KEY = "pk_test_YOUR_CLERK_PUBLISHABLE_KEY_HERE";
+// -----------------------------------------------------------
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </React.StrictMode>,
 )
