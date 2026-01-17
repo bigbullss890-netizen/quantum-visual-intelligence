4833f740ea617e02339b55a59228880be27e9f7d
bigbullss890-netizen <bigbullss890@gmail.com>
2026-01-18 02:38:34 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+// ADD THESE TWO LINES if you have the package installed
+// import '@carbon/styles/css/styles.css';
+// import '@carbon/styles/css/g100.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
